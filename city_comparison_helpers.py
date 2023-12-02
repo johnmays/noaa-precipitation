345f57afbc8973113fd0e8b1357b3b03cb4dabea
John Mays <25927827+johnmays@users.noreply.github.com>
2023-12-02 16:00:02 -0500

    checkpoint: successfully stitched dfs together

--- city_comparison_helpers.py
+++ city_comparison_helpers.py
@@ -1,5 +1,7 @@
 # Author: John Mays
 
+import pandas as pd
+
 def check_years(city_dict, csv_dir = './data/GSOY/', rng=(2012,2021)):
     ''' Iterates through CSVs and tells me which places are missing
         which years.
@@ -14,3 +16,26 @@ def check_years(city_dict, csv_dir = './data/GSOY/', rng=(2012,2021)):
                     print(city + " is missing years:")
                     any_missing_flag = True
                 print(year)
+
+
+def total_concat(city_dict, columns, csv_dir = './data/GSOY/',
+                rng=(2012, 2022)):
+    ''' Concatenates all of the individual cities' csvs together in one
+    data frame, adding a "CITY" column & restricting to the years
+    specified in (range)
+    '''
+    # initializing empty df:
+    total_df = None
+    # stitch all of the data frames together:
+    for i, (city, csv_name) in enumerate(city_dict.items()):
+        city_df = pd.read_csv((csv_dir + csv_name + ".csv"))
+        city_df = city_df[city_df['DATE'].isin(range(rng[0], (rng[1] + 1)))]
+        # add city name to DF:
+        city_df["CITY"] = city
+        if i == 0:
+            total_df = city_df
+        else:
+            total_df = pd.concat([total_df, city_df])
+    # restrict df to important columns only: 
+    total_df = total_df[(columns + ["CITY"])]
+    return total_df
